68eb4648ce47bbf74a3e1c4cbb646e9aa4e00147
Colin McNamara <coolinmc6@gmail.com>
2019-01-02 20:01:34 -0500

    Fix app.js with proper scope

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var version = 2;
+var version = 3;
 console.log('Version #' + version + ': App.js loaded');
 
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker
-		.register('/pwa-test/sw.js', {scope: '/'}) // you can control the scope
+		.register('/pwa-test/sw.js') // you can control the scope
 		.then(function() {
 			console.log('Service worker registered!')
 		})

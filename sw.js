a812b4592a97ce706c1b12b7abd5c947fa0f3ff2
Colin McNamara <coolinmc6@gmail.com>
2019-01-02 20:26:09 -0500

    icons updated

--- sw.js
+++ sw.js
@@ -1,8 +1,28 @@
+var cacheName = 'pwa-static-cache';
+var filesToCache = [
+  '/',
+  '/index.html'
+];
+
 self.addEventListener('install', function(event) {
 	console.log('[Service Worker] Installing Service Worker...', event)
+	event.waitUntil(
+	    caches.open(cacheName).then(function(cache) {
+	      console.log('[ServiceWorker] Caching app shell');
+	      return cache.addAll(filesToCache);
+	    })
+	  );
 });
 
 self.addEventListener('activate', function(event) {
 	console.log('[Service Worker] Activating Service Worker...', event)
 	return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });

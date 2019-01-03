78384fb68673f7fb63b0815492e87ff2dfffb94d
Colin McNamara <coolinmc6@gmail.com>
2019-01-02 19:53:30 -0500

    fix app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ console.log('App.js loaded');
 
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker
-		.register('/sw.js', {}) // you can control the scope
+		.register('/sw.js') // you can control the scope
 		.then(function() {
 			console.log('Service worker registered!')
 		});

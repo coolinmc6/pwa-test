d5c35f84802ed5e82a5008cc39d277aaad7a0014
Colin McNamara <coolinmc6@gmail.com>
2019-01-02 21:06:02 -0500

    prompt user to downloadgit status

--- js/app.js
+++ js/app.js
@@ -15,9 +15,32 @@ if('serviceWorker' in navigator) {
 }
 
 
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  // event.preventDefault();
-  // deferredPrompt = event;
-  return event;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  console.log(e);
 });
+
+
+$('.show-text').on('click', function() {
+	
+	if(deferredPrompt) {
+		deferredPrompt.prompt();
+
+		// Wait for the user to respond to the prompt
+		  deferredPrompt.userChoice
+		    .then((choiceResult) => {
+		      if (choiceResult.outcome === 'accepted') {
+		        console.log('User accepted the A2HS prompt');
+		      } else {
+		        console.log('User dismissed the A2HS prompt');
+		      }
+		      deferredPrompt = null;
+		    });
+	} else {
+		alert('Show text!');
+	}
+})
